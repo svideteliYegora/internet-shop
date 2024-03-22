94f835d0dd2dd038c531866fe8e9953314d85796
Egor K <112383203+GoshaCatharsis@users.noreply.github.com>
2024-03-22 17:42:36 +0300

    commit

--- app1/admin.py
+++ app1/admin.py
@@ -1,5 +1,45 @@
 from django.contrib import admin
-from .models import CategoryModel, GoodTypeModel
+from .models import CategoryModel, GoodTypeModel, OrderModel, OrderDetailModel, GoodsOnWarehouseModel, WarehouseModel
 
-admin.site.register(CategoryModel)
-admin.site.register(GoodTypeModel)
+
+class GoodTypeInline(admin.TabularInline):
+    model = GoodTypeModel.categories.through
+
+
+@admin.register(GoodTypeModel)
+class GoodTypeAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "get_categories_display", "is18", "image"]
+    fields = ["name", "categories", "description", "image", "is18"]
+    list_filter = ["name", "categories", "is18"]
+
+    def get_categories_display(self, obj):
+        return ", ".join([category.name for category in obj.categories.all()])
+    get_categories_display.short_description = 'Categories'
+
+
+@admin.register(OrderModel)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "total_price", "order_date", "status"]
+    list_filter = ["order_date", "status"]
+
+
+@admin.register(CategoryModel)
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [
+        GoodTypeInline,
+    ]
+
+
+@admin.register(OrderDetailModel)
+class OrderDetailAdmin(admin.ModelAdmin):
+    list_display = ["order_id", "quantity", "good_type_id"]
+
+
+@admin.register(GoodsOnWarehouseModel)
+class GoodsOnWarehouseAdmin(admin.ModelAdmin):
+    list_display = ["good_type_id", "warehouse_id"]
+
+
+@admin.register(WarehouseModel)
+class WarehouseAdmin(admin.ModelAdmin):
+    list_display = ["address", "warehouse_name"]
--- app1/models.py
+++ app1/models.py
@@ -1,20 +1,6 @@
 import uuid
 import datetime
 from django.db import models
-from django.contrib.auth.models import User
-
-
-class GoodTypeModel(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    name = models.CharField(max_length=50)
-    category = models.ForeignKey("CategoryModel", null=True, on_delete=models.SET_NULL)
-    description = models.TextField()
-    image = models.ImageField()
-    is18 = models.BooleanField()
-    expiration_date = models.DurationField(null=True, default=datetime.timedelta(days=730))
-
-    def __str__(self):
-        return self.name
 
 
 class CategoryModel(models.Model):
@@ -25,28 +11,50 @@ class CategoryModel(models.Model):
         return self.name
 
 
+class GoodTypeModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=50)
+    description = models.TextField(blank=True, null=True)
+    categories = models.ManyToManyField(CategoryModel)
+    image = models.ImageField(blank=True, null=True)
+    is18 = models.BooleanField()
+    expiration_date = models.DurationField(null=True, default=datetime.timedelta(days=730), blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+# class GoodTypeCategoryModel(models.Model):
+#     good_type_id = models.ForeignKey("GoodTypeModel", null=True, on_delete=models.SET_NULL)
+#     category_id = models.ForeignKey("CategoryModel", null=True, on_delete=models.SET_NULL)
+
+
 class OrderModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     total_price = models.DecimalField(max_digits=5, decimal_places=2)
-    order_date = models.DateTimeField()
-    user_id = models.ForeignKey("User", on_delete=models.SET_NULL)
+    order_date = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=50)
-    number = models.AutoField()
 
     def __str__(self):
-        return f"Order #{self.number}"
+        return f"Order #{self.id} + {self.order_date}"
 
 
 class OrderDetailModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    order_id = models.ForeignKey("OrderModel", on_delete=models.SET_NULL)
+    order_id = models.ForeignKey("OrderModel", on_delete=models.SET_NULL, null=True)
     quantity = models.IntegerField()
-    good_type_id = models.ForeignKey("GoodTypeModel", on_delete=models.SET_NULL)
+    good_type_id = models.ForeignKey("GoodTypeModel", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return str(self.order_id)
 
 
 class GoodsOnWarehouseModel(models.Model):
-    good_type_id = models.ForeignKey("GoodTypeModel", on_delete=models.SET_NULL)
-    warehouse_id = models.ForeignKey("WarehouseModel", on_delete=models.SET_NULL)
+    good_type_id = models.ForeignKey("GoodTypeModel", on_delete=models.SET_NULL, null=True)
+    warehouse_id = models.ForeignKey("WarehouseModel", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return str(self.warehouse_id)
 
 
 class WarehouseModel(models.Model):
